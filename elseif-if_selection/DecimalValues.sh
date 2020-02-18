05ac06ebe2d3a5058b0e5d235c9222d6c9dc2898
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-02-18 17:11:21 +0530

    Add DecimalValues.sh

---
+++ elseif-if_selection/DecimalValues.sh
@@ -0,0 +1,72 @@
+echo "Enter a number less/equalto 9999:"
+read num
+if((${#num}==4))
+then
+
+	if((${#num}==4))
+	then
+		temp=$((num%10))
+		echo "$temp-unit"
+		num=$((num/10))
+	fi
+	if((${#num}==3))
+	then
+		temp=$((num%10))
+        	echo "$temp tenth"
+        	num=$((num/10))
+	fi
+	if((${#num}==2))
+	then
+        	temp=$((num%10))
+        	echo "$temp hundred"
+        	num=$((num/10))
+	fi
+	if((${#num}==1))
+	then
+        	temp=$((num%10))
+        	echo "$temp Thousand"
+        	num=$((num/10))
+	fi
+
+elif((${#num}==3))
+then
+	if((${#num}==3))
+	then
+        	temp=$((num%10))
+        	echo "$temp unit"
+        	num=$((num/10))
+	fi
+	if((${#num}==2))
+	then
+                temp=$((num%10))
+                echo "$temp tenth"
+                num=$((num/10))
+        fi
+	if((${#num}==1))
+	then
+                temp=$((num%10))
+                echo "$temp hundred"
+                num=$((num/10))
+        fi
+
+elif((${#num}==2))
+then
+	if((${#num}==2))
+	then
+                temp=$((num%10))
+                echo "$temp unit"
+                num=$((num/10))
+        fi
+	if((${#num}==1))
+	then
+                temp=$((num%10))
+                echo "$temp tenth"
+                num=$((num/10))
+        fi
+elif((${#num}==1))
+then
+	echo "$num unit"
+else
+	echo "Out of range"
+fi
+
