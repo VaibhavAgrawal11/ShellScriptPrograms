c44d7679e30f9cca50dbef7364089d9494604255
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-02-18 19:33:26 +0530

    UnitConversion.sh

---
+++ case_selection/UnitConversion.sh
@@ -0,0 +1,28 @@
+echo "Enter Num between 1 to 4"
+read num
+case $num in
+	1)
+	echo "Conversion of Feet into Inch"
+	echo "Enter feet: "
+	read feet
+	inch=$((feet*12))
+	echo "Inches: $inch";;
+	2)
+	echo "Conversion Inch to Feet"
+        echo "Enter inches: "
+        read inch
+        feet=$((inch/12))
+        echo "feet: $feet";;
+	3)
+	echo "Conversion of Feet into Meter"
+        echo "Enter feet: "
+        read feet
+        meter=`echo "scale=3 ; $feet/3.218" | bc -l`
+        echo "Meters : $meter";;
+	4)
+	echo "Conversion of Meter into Feet"
+        echo "Enter Meters: "
+        read meter
+        feet=`echo "scale=3; $meter*3.218" | bc`
+        echo "Feet: $feet";;
+esac
