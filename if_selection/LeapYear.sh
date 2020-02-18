c1dd1e4128f933c48ddcdb1f583b505184c85f29
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-02-18 17:21:19 +0530

    Add LeapYear.sh

---
+++ if_selection/LeapYear.sh
@@ -0,0 +1,13 @@
+echo "Enter year"
+read year
+if(( year > 999 ))
+then
+	if(( $year%400 == 0 || $year%4 == 0 && $year%100 !=0 ))
+	then
+		echo "leap year"
+ 	else
+		echo "Not a Leap Year"
+	fi
+else
+	echo "Invalid Input"
+fi
