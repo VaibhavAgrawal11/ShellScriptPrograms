d8d14aa5b797afce0555c1e11bf59af988f8559e
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-02-18 13:01:03 +0530

    Add AreaInAcres

---
+++ sequence_practice/unit_conversion/AreaInAcres.sh
@@ -0,0 +1,6 @@
+lengthFt=60
+breadthFt=40
+lengthM=`echo "scale=2; $lengthFt/3.218" | bc -l`
+breadthM=`echo "scale=2; $breadthFt/3.218" | bc -l`
+area=`echo "scale=4; $lengthM * $breadthM *25 /4047" |bc -l` 
+echo "$area is area in Acres"
