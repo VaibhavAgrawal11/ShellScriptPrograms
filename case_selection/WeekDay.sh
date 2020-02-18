1cc199ab3fc5cac3bb3502732655c733db8339e6
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-02-18 19:32:39 +0530

    WeekDay.sh

---
+++ case_selection/WeekDay.sh
@@ -0,0 +1,20 @@
+echo "Enter the week Number: "
+read weekday
+case $weekday in 
+	0)
+	echo "Sunday";;
+	1)
+	echo "Monday";;
+	2)
+        echo "Tuesday";;
+	3)
+        echo "Wednesday";;
+	4)
+        echo "Thrusday";;
+	5)
+        echo "Friday";;
+	6)
+        echo "Saturday";;
+	*)
+	echo "Invalid";;
+esac
