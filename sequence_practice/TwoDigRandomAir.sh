714a7206283e03e80245dfd00741cb0632d13649
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-02-18 12:52:37 +0530

    Add TwoDigitRandomAir.sh

---
+++ sequence_practice/TwoDigRandomAir.sh
@@ -0,0 +1,13 @@
+rand1=$((RANDOM%90+10))
+echo $rand1
+rand2=$((RANDOM%90+10))
+echo $rand2
+rand3=$((RANDOM%90+10))
+echo $rand3
+rand4=$((RANDOM%90+10))
+echo $rand4
+rand5=$((RANDOM%+90+10))
+echo $rand5
+sum=$((rand1+rand2+rand3+rand4+rand5))
+echo sum is $sum
+echo "scale=2;  $sum/5" | bc -l
