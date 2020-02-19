690f7fb6b6742dd8241c3a2cb3c571d891bcc4e6
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-02-19 12:05:55 +0530

    add PrimeNumber.sh

---
+++ for_loop_rep/PrimeNumber.sh
@@ -0,0 +1,21 @@
+echo "Enter a Number"
+read  number
+flag=0
+if(($number==1))
+then
+	flag=1
+fi
+
+for((iter=2; iter<=$((number/2)); iter++))
+do
+	if(($((number%iter))==0))
+	then
+		flag=1
+	fi
+done
+if(($flag==0))
+then
+	echo "Prime Number"
+else
+	echo "Not Prime Number"
+fi
