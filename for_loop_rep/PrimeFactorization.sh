7e1b99a89ac473c0d2493e504cb4b5ceae45f69e
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-02-19 12:06:50 +0530

    add PrimeFactorization.sh

---
+++ for_loop_rep/PrimeFactorization.sh
@@ -0,0 +1,29 @@
+echo "Enter a number:"
+read number
+temp=$number
+for((iter=1; iter<=$((number/2)); iter++))
+do
+	flag=0
+
+	if(($iter==1))
+	then
+		flag=1
+	fi
+
+	for((iter2=2; iter2<=$((iter/2)); iter2++))
+	do
+		if(($((iter%iter2))==0))
+		then
+			flag=1
+		fi
+	done
+	if(($flag==0))
+	then
+		if(($((temp%iter))==0))
+		then
+			echo $iter
+			temp=$((temp/iter))
+			iter=$((iter-1))
+		fi
+	fi
+done
