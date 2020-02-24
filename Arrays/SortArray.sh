f99ce955f6222d809971f9dcb33fc3eee6a191b3
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-02-24 15:21:26 +0530

    MODIFIED: Added logical comments

--- Arrays/SortArray.sh
+++ Arrays/SortArray.sh
@@ -1,21 +1,23 @@
 #!/bin/bash -x
+#RANDOMLY GENERATE TEN 3 DIGIT NUMBER AND STORE THEM IN A ARRAY 
 for(( iter=0; iter<10; iter++))
 do
         random=$((RANDOM%900+100))
         array[$iter]=$random
 done
 echo ${array[@]}
+#SORTING THE ARRAY WITH NESTED FORLOOP
 for(( iter=0; iter<10; iter++))
 do
-        for(( nested=0; nested<10; nested++))
+   for(( nested=0; nested<10; nested++))
 	do
 		if((${array[iter]}<${array[nested]}))
 		then
 			temp=${array[iter]}
 			array[$iter]=${array[nested]}
 			array[$nested]=$temp
-		fi	done
-
+		fi
+	done
 done
 echo "Sorted Array: "
 echo ${array[@]}
