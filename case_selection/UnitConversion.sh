2a5846f14d0690c5049e16d6e33f00ad55d9c2a4
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-02-24 16:23:43 +0530

    MODIFIED: Added logical comments

--- case_selection/UnitConversion.sh
+++ case_selection/UnitConversion.sh
@@ -3,24 +3,28 @@ echo "Enter Num between 1 to 4"
 read num
 case $num in
 	1)
+	#CONVERT FEET TO INCH
 	echo "Conversion of Feet into Inch"
 	echo "Enter feet: "
 	read feet
 	inch=$((feet*12))
 	echo "Inches: $inch";;
 	2)
+	#CONVERT INCH TO FEET
 	echo "Conversion Inch to Feet"
         echo "Enter inches: "
         read inch
         feet=$((inch/12))
         echo "feet: $feet";;
 	3)
+	#CONVERT FEET TO INCH
 	echo "Conversion of Feet into Meter"
         echo "Enter feet: "
         read feet
         meter=`echo "scale=3 ; $feet/3.218" | bc -l`
         echo "Meters : $meter";;
 	4)
+	#CONVERT METER TO FEET
 	echo "Conversion of Meter into Feet"
         echo "Enter Meters: "
         read meter
